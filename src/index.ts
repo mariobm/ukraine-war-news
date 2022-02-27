95691d2c411544b4c145552c28677f9ca820fef9
Mario <mariob@vivaldi.net>
2022-02-27 14:28:44 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Client } from "discord.js";
+import { IntentOptions } from "./config/IntentOptions.js";
+
+(async () => {
+  const BOT = new Client({ intents: IntentOptions });
+  BOT.on("ready", () => console.log("Connected to Discord!"));
+  await BOT.login(process.env.BOT_TOKEN);
+})();
